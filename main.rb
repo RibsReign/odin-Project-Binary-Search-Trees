5fd11214a785ab69274b3a419774bc7e209a40b5
Ondrej Hulík <hulik.ondrej@gmail.com>
2025-01-16 20:09:39 +0100

    chore: add insert test cases

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 require_relative 'libs/tree'
-unsorted_tree = Tree.new
+tree = Tree.new
 
-unsorted_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-unsorted_tree.visualize_tree
+tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+tree.insert(1)
+tree.insert(9000)
+tree.insert(6)
+tree.visualize_tree
 
 # unsorted_tree = Tree.new
 # unsorted_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 5, 7, 9, 67, 6345, 324])
