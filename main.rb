ab3f586db314dc211b693ece183ce8ff8df32a08
Ondrej Hulík <hulik.ondrej@gmail.com>
2025-01-19 11:37:58 +0100

    chore: add delete test cases

--- main.rb
+++ main.rb
@@ -9,6 +9,11 @@ tree.insert(1)
 tree.insert(9000)
 tree.insert(6)
 tree.visualize_tree
+tree.delete(5)
+tree.visualize_tree
+
+tree.delete(6)
+tree.visualize_tree
 
 # unsorted_tree = Tree.new
 # unsorted_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 5, 7, 9, 67, 6345, 324])
