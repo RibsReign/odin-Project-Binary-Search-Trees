a135a8bad0774d26f23efde49f00d99a709c6be8
Ondrej Hulík <hulik.ondrej@gmail.com>
2025-01-21 17:43:10 +0100

    chore: add height test case

--- main.rb
+++ main.rb
@@ -49,6 +49,7 @@ tree.visualize_tree
 tree.postorder do |node|
   puts "Yielded node: #{node.value}, Left: #{node.left&.value}, Right: #{node.right&.value}"
 end
+p tree.height(1)
 # unsorted_tree = Tree.new
 # unsorted_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 5, 7, 9, 67, 6345, 324])
 # unsorted_tree.visualize_tree
