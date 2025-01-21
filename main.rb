2e1cbe972e00255d18d50dad90b5d56276f0a236
Ondrej Hulík <hulik.ondrej@gmail.com>
2025-01-21 16:26:09 +0100

    chore: add level_order case

--- main.rb
+++ main.rb
@@ -21,21 +21,24 @@ tree.visualize_tree
 tree.delete(8)
 tree.visualize_tree
 
-tree_two = Tree.new
-tree_two.build_tree([33, 664, 387, 442, 414, 892, 313, 931, 134, 362,
-                     396, 325, 666, 676, 746, 823, 836, 541, 590, 622,
-                     78, 873, 632, 21, 159, 359, 915, 451, 524, 952,
-                     760, 122, 485, 528, 963, 224, 820, 694, 269, 594])
-tree_two.visualize_tree
-tree_two.delete(528)
-tree_two.visualize_tree
-tree_two.delete(963)
-tree_two.visualize_tree
-tree_two.delete(952)
-tree_two.visualize_tree
-tree_two.delete(892)
-tree_two.visualize_tree
-p tree_two.find(33)
+# tree_two = Tree.new
+# tree_two.build_tree([33, 664, 387, 442, 414, 892, 313, 931, 134, 362,
+#                      396, 325, 666, 676, 746, 823, 836, 541, 590, 622,
+#                      78, 873, 632, 21, 159, 359, 915, 451, 524, 952,
+#                      760, 122, 485, 528, 963, 224, 820, 694, 269, 594])
+# tree_two.visualize_tree
+# tree_two.delete(528)
+# tree_two.visualize_tree
+# tree_two.delete(963)
+# tree_two.visualize_tree
+# tree_two.delete(952)
+# tree_two.visualize_tree
+# tree_two.delete(892)
+# tree_two.visualize_tree
+# p tree_two.find(33)
+tree.level_order do |node|
+  puts "Yielded node: #{node.value}, Left: #{node.left&.value}, Right: #{node.right&.value}"
+end
 # unsorted_tree = Tree.new
 # unsorted_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 5, 7, 9, 67, 6345, 324])
 # unsorted_tree.visualize_tree
