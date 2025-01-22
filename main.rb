452c3b7769bdb5355ea8c92d5cd8bc960086349f
Ondrej Hulík <hulik.ondrej@gmail.com>
2025-01-22 16:28:03 +0100

    chore: add rebalance test case

--- main.rb
+++ main.rb
@@ -63,6 +63,8 @@ tree.postorder do |node|
 end
 
 p tree.balanced?
+tree = tree.rebalance
+tree.visualize_tree
 # unsorted_tree = Tree.new
 # unsorted_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 5, 7, 9, 67, 6345, 324])
 # unsorted_tree.visualize_tree
